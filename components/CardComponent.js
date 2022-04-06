b26d99d79278a02b913b432d0d61d9fc059e9490
niyoh55 <niyohedwyn21@gmail.com>
2022-04-07 00:14:11 +0800

    Changed Styling

--- components/CardComponent.js
+++ components/CardComponent.js
@@ -7,35 +7,8 @@ const CardComponent = (props) => {
   const router = useRouter();
 
   const { imdbID, Images, Title, Plot } = props.movies;
-  const [isClicked, setIsClicked] = useState(false);
 
   const [imgCounter, setImgCounter] = useState(0);
-
-  // const [imgCounter, setImgCounter] = useState(0);
-
-  // let intervalID;
-
-  // function changeImage() {
-  //   if (imgCounter < Images.length - 1) {
-  //     setImgCounter((imgCounter) => imgCounter + 1);
-  //     console.log(imgCounter);
-  //   } else if (imgCounter == Images.length - 1) {
-  //     console.log(imgCounter + ": nice");
-  //     setImgCounter(0);
-  //   }
-  // }
-
-  // const hoverCard = () => {
-  //   // check if already an interval has been set up
-  //   intervalID = setInterval(changeImage, 1000);
-
-  //   console.log("enter");
-  // };
-
-  // const exitHoverCard = () => {
-  //   console.log("exit");
-  //   return clearInterval(intervalID);
-  // };
 
   const enterMouse = () => {
     let val = Math.floor(Math.random() * Images.length);
@@ -47,19 +20,18 @@ const CardComponent = (props) => {
     <Card
       onMouseEnter={enterMouse}
       onMouseLeave={() => setImgCounter(0)}
-      radius={5}
       onClick={() => router.push(`/` + imdbID.toString())}
-      className="bg-gray-400 h-[550px]"
+      className="bg-gray-100 h-[35rem] rounded-xl"
     >
       <Card.Section>
-        <Image src={Images[imgCounter]} height={400} fit="cover" />
+        <Image
+          src={Images.length === 1 ? Images[0] : Images[imgCounter]}
+          height={400}
+          fit="cover"
+        />
       </Card.Section>
-
-      <Text weight={500} size="lg" mt={20}>
-        {Title}
-      </Text>
-
-      <Text size="sm">{Plot}</Text>
+      <p className="text-4xl my-10 font-bold tracking-tight">{Title}</p>
+      {/* <p className="text-base">{Plot}</p> */}
     </Card>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -13,36 +13,25 @@ const HomePage = (props) => {
 
   const displayMeetups = () => {
     return movies.map((x) => (
-      <Grid.Col
-        key={x._id.toString()}
-        xs={12}
-        sm={6}
-        md={4}
-        lg={3}
-        className="hover:scale-[120%] hover:z-20 duration-200 	"
-      >
-        <div className="flex-row justify-center items-center hover:shadow-2xl">
-          <CardComponent
-            movies={{
-              imdbID: x.imdbID,
-              Title: x.Title,
-              Images: x.Images,
-              Plot: x.Plot,
-              ID: x._id,
-            }}
-          />
-        </div>
-      </Grid.Col>
+      <div className=" hover:scale-[110%] active:scale-[150%] hover:z-20 duration-200 flex-row justify-center items-center hover:shadow-2xl">
+        <CardComponent
+          movies={{
+            imdbID: x.imdbID,
+            Title: x.Title,
+            Images: x.Images,
+            Plot: x.Plot,
+            ID: x._id,
+          }}
+        />
+      </div>
     ));
   };
 
-  console.log(process.env.DB_HOST);
-
   return (
     <div className="px-5 overflow-hidden">
-      <Grid gutter="xl" style={{ marginTop: "20px" }}>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-10 mt-10 ">
         {displayMeetups()}
-      </Grid>
+      </div>
     </div>
   );
 };
@@ -91,6 +80,7 @@ export async function getStaticProps(context) {
     props: {
       movies: nice,
     },
+    revalidate: 10,
   };
 }
 
